a7777b668cf30e3adf51f6cacad333d976b7a437
juno-choi <ililil9482@naver.com>
2023-04-20 17:32:36 +0900

    [기능] 간단한 배치 테스트 추가

---
+++ src/main/java/com/juno/hyugibatch/repository/HyugiJobRepository.java
@@ -0,0 +1,8 @@
+package com.juno.hyugibatch.repository;
+
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HyugiJobRepository extends JobRepository {
+}
