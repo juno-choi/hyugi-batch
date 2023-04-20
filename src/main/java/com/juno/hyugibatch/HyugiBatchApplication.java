ebd242756e9101a57bfd1b8c2fa1341f94b667e8
juno-choi <ililil9482@naver.com>
2023-04-20 13:53:17 +0900

    [설정] 프로젝트 시작

---
+++ src/main/java/com/juno/hyugibatch/HyugiBatchApplication.java
@@ -0,0 +1,13 @@
+package com.juno.hyugibatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyugiBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyugiBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juno/hyugibatch/HyugiBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.hyugibatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyugiBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
